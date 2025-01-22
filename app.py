c1d36393bce4f4b5e5ac2850c0b524553763f692
Brandon Hall <Brandon.Hall@qusmart.ai>
2025-01-22 17:06:36 -0500

    prep for deployment

--- app.py
+++ app.py
@@ -397,7 +397,5 @@ def update_location():
         return jsonify({"error": "An unexpected error occurred."}), 500
 
 
-
-
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
