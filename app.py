720d44f54109a5807afcb5b87e7994a5fd902145
Brandon Hall <Brandon.Hall@qusmart.ai>
2025-01-22 17:47:39 -0500

    bugfix

--- app.py
+++ app.py
@@ -11,15 +11,24 @@ from firebase_admin import credentials, db
 import base64
 import time
 
-# Path to the credentials file
-CREDENTIALS_PATH = "firebase\creds.json"  # Replace with the actual path
 
-# Initialize Firebase Admin SDK
-if not firebase_admin._apps:
-    cred = credentials.Certificate(CREDENTIALS_PATH)
-    firebase_admin.initialize_app(cred, {
-        'databaseURL': 'https://weatherapp-27614-default-rtdb.firebaseio.com/'
-    })
+firebase_config = {
+    "type": "service_account",
+    "project_id": os.getenv("FIREBASE_PROJECT_ID"),
+    "private_key_id": os.getenv("FIREBASE_PRIVATE_KEY_ID"),
+    "private_key": os.getenv("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
+    "client_email": os.getenv("FIREBASE_CLIENT_EMAIL"),
+    "client_id": os.getenv("FIREBASE_CLIENT_ID"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+    "client_x509_cert_url": f"https://www.googleapis.com/robot/v1/metadata/x509/{os.getenv('FIREBASE_CLIENT_EMAIL')}",
+}
+
+cred = credentials.Certificate(firebase_config)
+firebase_admin.initialize_app(cred, {
+    'databaseURL': 'https://weatherapp-27614-default-rtdb.firebaseio.com/'
+})
 
 # Load environment variables
 load_dotenv()
